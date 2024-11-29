6346298393aee846f8e41c949cf3f2e8f104d26e
Evelyn <53948799+EvelynSays@users.noreply.github.com>
2024-11-29 17:06:20 +0000

    Enhance layout component with tooltips for navigation links

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,22 +1,33 @@
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
-   return (
-   <>
-    <header className="bg-indigo-500 text-white">
-            <nav className="flex items-center justify-between p-4">
-                <Link to="/" className="fa-solid fa-house nav-link"></Link>
-                <div className="flex items-center gap-2">
-                    <Link to="/login" className="fa-solid fa-right-to-bracket nav-link"></Link>
-                    <Link to="/register" className="fa-solid fa-user-plus nav-link"></Link>
-                </div>
-            </nav>
-        </header>
+    return (
+        <>
+            <header className="bg-indigo-500 text-white">
+                <nav className="flex items-center justify-between p-4">
+                    <Link
+                        to="/"
+                        className="fa-solid fa-house nav-link"
+                    ></Link>
+                    <div className="flex items-center gap-2">
+                        <Link
+                            title="Login"
+                            to="/login"
+                            className="fa-solid fa-right-to-bracket nav-link"
+                        ></Link>
+                        <Link
+                            title="Register"
+                            to="/register"
+                            className="fa-solid fa-user-plus nav-link"
+                        ></Link>
+                    </div>
+                </nav>
+            </header>
 
-        <main className="p-4">
-            <Outlet />
-        </main>
-    </>
+            <main className="p-4">
+                <Outlet />
+            </main>
+        </>
     )
 };
 
