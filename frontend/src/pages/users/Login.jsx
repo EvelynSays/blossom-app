5558cf64874d34d73ba7f58f6dcf4f7e17840af7
Evelyn <53948799+EvelynSays@users.noreply.github.com>
2024-11-29 16:20:56 +0000

    Implement login and registration forms with error handling and alert component

--- frontend/src/pages/users/Login.jsx
+++ frontend/src/pages/users/Login.jsx
@@ -1,12 +1,46 @@
+import { useState } from "react";
+import Alert from "../../components/Alert";
+
 const Login = () => {
+
+    // Error State
+    const [error, setError] = useState(null);
+
+    // Form data state
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    // Handle login form submission
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
     return (
         <section className="card">
             <h1 className="title">Login to your account</h1>
-            <form>
-                <input type="email" placeholder="Email Address"  className="input" autoFocus />
-                <input type="password" placeholder="Password"  className="input" />
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email Address"
+                    className="input"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                    autoFocus
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="input"
+                    value={formData.password}
+                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
                 <button className="btn">Login</button>
             </form>
+            {error && <Alert msg={error} />}
+
         </section>
     );
 };
--- frontend/src/pages/users/Register.jsx
+++ frontend/src/pages/users/Register.jsx
@@ -1,8 +1,55 @@
+import { useState } from 'react';
+import Alert from '../../components/Alert';
+
 const Register = () => {
+    // Error State
+    const [error, setError] = useState(null);
+
+    // Form data state
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    });
+
+
+    // Handle login form submission
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    };
+
     return (
-        <div>
-            <h1>Register</h1>
-        </div>
+        <section className="card">
+            <h1 className="title">Create a new account</h1>
+            <form onSubmit={handleRegister}>
+                <input
+                    type="email"
+                    placeholder="Email Address"
+                    className="input"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                    autoFocus
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    className="input"
+                    value={formData.password}
+                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <input
+                    type="password"
+                    placeholder="Confirm Password"
+                    className="input"
+                    value={formData.passwordConfirm}
+                    onChange={(e) => setFormData({ ...formData, passwordConfirm: e.target.value })}
+                />
+                <button className="btn">Login</button>
+            </form>
+            {error && <Alert msg={error} />}
+
+        </section>
     );
 };
 
