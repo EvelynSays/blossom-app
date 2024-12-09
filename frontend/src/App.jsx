756bd4c774226d417e8f4320db6ecd94298f01d8
Evelyn <53948799+EvelynSays@users.noreply.github.com>
2024-12-09 10:39:48 +0000

    Add authentication and guest routing; implement AuthRoutes and GuestRoutes components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,18 +6,27 @@ import Dashboard from './pages/users/Dashboard';
 import Home from './pages/posts/Home';
 import Create from './pages/posts/Create';
 import Update from './pages/posts/Update';
+import AuthRoutes from './routes/AuthRoutes';
+import GuestRoutes from './routes/GuestRoutes';
 
 const App = () => {
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} /> 
-        <Route path="dashboard" element={<Dashboard />} /> 
-        <Route path="create" element={<Create />} /> 
-        <Route path="update" element={<Update />} /> 
-      </Route> 
+
+        <Route element={<AuthRoutes />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="create" element={<Create />} />
+          <Route path="update" element={<Update />} />
+        </Route>
+
+        <Route element={<GuestRoutes />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        
+      </Route>
     </Routes>
   </BrowserRouter>
 };
